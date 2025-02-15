cf27a8a8a76da14741d3c912dfe40da7de360ace
sxye <1251221361@qq.com>
2025-02-15 07:41:09 +0000

    mdf check

--- AFTCS/Test_checkcom.cpp
+++ AFTCS/Test_checkcom.cpp
@@ -2,7 +2,7 @@
 #include <mutex>
 #include <thread>
 
-constexpr int MAXN = 100000;
+int MAXN = 100000;
 
 struct checkstr{
     int n, m, S, k, d;
@@ -66,47 +66,52 @@ checkstr check_community(std::ifstream &fin, int q, int F){
         fin >> vertices[i];
         p[vertices[i]] = i;
     }
-    if(m == 0 || m > MAXN){
-        checkstr res;
-        res.d = -1;
-        return res;
-    }
-
-    // 多线程计算直径
-    global_max_d = 0;
-
-    // 确定合适的线程数，通常是系统硬件线程数
-    int num_threads = std::min<int>({std::thread::hardware_concurrency(), n, 16});
-    int vertices_per_thread = n / num_threads;  // 每个线程处理的顶点数
-    int remaining_vertices = n % num_threads;   // 处理不均分的顶点
-
-    std::vector<std::thread> threads;
-
-    int start_idx = 0;
-    for(int t = 0; t < num_threads; ++t) {
-        int end_idx = start_idx + vertices_per_thread + (t < remaining_vertices ? 1 : 0);  // 分配额外的顶点给前 remaining_vertices 个线程
-        threads.emplace_back(bfs_diameter_range, start_idx, end_idx, std::cref(vertices), std::cref(adj), std::cref(p));
-        start_idx = end_idx;
-    }
-
-    // 等待所有线程完成
-    for(auto &t : threads){
-        t.join();
-    }
 
     checkstr res;
-    res.d = global_max_d;
     res.n = n;
     res.m = m;
     res.S = S;
     res.k = k;
+    res.d = -1;
+    res.t = -1;
+
+    if(m == 0){
+        return res;
+    }else if(m < MAXN){
+        // 多线程计算直径
+        global_max_d = 0;
+    
+        // 确定合适的线程数，通常是系统硬件线程数
+        int num_threads = std::min<int>({(int) std::thread::hardware_concurrency(), n, 16});
+        int vertices_per_thread = n / num_threads;  // 每个线程处理的顶点数
+        int remaining_vertices = n % num_threads;   // 处理不均分的顶点
+    
+        std::vector<std::thread> threads;
+    
+        int start_idx = 0;
+        for(int t = 0; t < num_threads; ++t) {
+            int end_idx = start_idx + vertices_per_thread + (t < remaining_vertices ? 1 : 0);  // 分配额外的顶点给前 remaining_vertices 个线程
+            threads.emplace_back(bfs_diameter_range, start_idx, end_idx, std::cref(vertices), std::cref(adj), std::cref(p));
+            start_idx = end_idx;
+        }
+    
+        // 等待所有线程完成
+        for(auto &t : threads){
+            t.join();
+        }
+        res.d = global_max_d;
+    }
+
     fin >> res.t; // time us
     return res;
 }
 
 int main(int argc, char *argv[]){
-    if(argc != 3){
-        std::cerr << "main: 2 argument!\n";
+    if(argc == 4){
+        MAXN = atoi(argv[3]);
+    }else if(argc != 3){
+        //用法
+        std::cout << "Usage: ./check_community <query> <output_dir> [MAXN]\n";
         exit(1);
     }
     // 是否有解，是否爆了，直径多少
@@ -123,10 +128,6 @@ int main(int argc, char *argv[]){
         }
         std::ifstream fin(str);
         auto cks = check_community(fin, q, F);
-        if(cks.d != -1){
-            fout << q << " " << F << " " << R << " " << b << " d: " << cks.d << " n: " << cks.n << " m: " << cks.m << " S: " << cks.S << " k: " << cks.k << " t: " << 1.0 * cks.t / (1e6) << "\n";
-        }else{
-            fout << q << " " << F << " " << R << " " << b << " -1\n";
-        }
+        fout << q << " " << F << " " << R << " " << b << " d: " << cks.d << " n: " << cks.n << " m: " << cks.m << " S: " << cks.S << " k: " << cks.k << " t: " << 1.0 * cks.t / (1e6) << "\n";
     }
 }
